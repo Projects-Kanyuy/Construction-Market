6319203aad6cc36c99e06a243226dc07b6362c5c
zaza203 <ihimbruzadolf@gmail.com>
2025-05-24 13:23:35 +0100

    fixes

--- src/api/axiosInstance.ts
+++ src/api/axiosInstance.ts
@@ -1,8 +1,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-  // baseURL: 'https://api.cpromart.site/api',
-  baseURL: 'http://localhost:5000/api',
+  baseURL: 'https://api.cpromart.site/api',
   withCredentials: false,
 });
 
